16edb0effe17f164901a3fc52465a2aca474ef02
OSG Development <osgdevelopment56@gmail.com>
2025-08-26 11:15:11 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 game 'rdr3'
 
 description 'VORP-compatible NPC spawner (fork of rsg-npcs)'
-version '2.0.5-vorp'
+version '2.0'
 
 shared_scripts {
     'config.lua'
@@ -30,4 +30,4 @@ dependencies {
 }
 
 lua54 'yes'
-version '2.0'
+
