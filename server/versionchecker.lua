2dff9cef661c068e2dfcfc1f5eaba458da5e0f33
OSG Development <osgdevelopment56@gmail.com>
2025-08-26 11:33:38 -0400

    Update versionchecker.lua

--- server/versionchecker.lua
+++ server/versionchecker.lua
@@ -1,15 +1,10 @@
 -----------------------------------------------------------------------
 -- version checker
 -----------------------------------------------------------------------
-local function versionCheckPrint(_type, log)
-    local color = _type == 'success' and '^2' or '^1'
-    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
-end
-
 local function CheckVersion()
     local resource = GetCurrentResourceName()
     local currentVersion = GetResourceMetadata(resource, 'version', 0)
-    local githubURL = 'https://raw.githubusercontent.com/osgdevelopment/osg_npcs/refs/heads/main/version'
+    local githubURL = 'https://raw.githubusercontent.com/osgdevelopment/osg_npcs/refs/heads/main/version?t=' .. os.time()
 
     PerformHttpRequest(githubURL, function(err, text, headers)
         if err ~= 200 or not text then
