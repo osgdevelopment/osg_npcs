d25016d21d02f63d5911608d8d1a021a4a78e0eb
OSG Development <osgdevelopment56@gmail.com>
2025-08-26 11:34:54 -0400

    Update versionchecker.lua

--- server/versionchecker.lua
+++ server/versionchecker.lua
@@ -1,9 +1,15 @@
 -----------------------------------------------------------------------
 -- version checker
 -----------------------------------------------------------------------
+local function versionCheckPrint(_type, log)
+    local color = _type == 'success' and '^2' or '^1'
+    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
+end
+
 local function CheckVersion()
     local resource = GetCurrentResourceName()
     local currentVersion = GetResourceMetadata(resource, 'version', 0)
+    -- add ?t=os.time() to bust GitHub cache
     local githubURL = 'https://raw.githubusercontent.com/osgdevelopment/osg_npcs/refs/heads/main/version?t=' .. os.time()
 
     PerformHttpRequest(githubURL, function(err, text, headers)
@@ -12,7 +18,7 @@ local function CheckVersion()
             return
         end
 
-        -- get only the first line (the actual version number)
+        -- only take the first line (the version number)
         local latestVersion = text:match("([^\r\n]+)")
 
         versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
